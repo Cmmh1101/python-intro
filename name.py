a17f23e1cc2114d3151336a739247c4785c212c5
Carla Montano <cm.emprendiendo@gmail.com>
2023-01-25 23:28:34 -0600

    initial commit

---
+++ name.py
@@ -0,0 +1,7 @@
+name = input('Name:')
+
+# print('Hello, ' + name)  without formatting string
+
+# using newer way with formatting string like template literal
+print(f"Hello, {name}")
+mood = input(f'how are you today?')
---
+++ variables.py
@@ -0,0 +1,5 @@
+a = 28     # num
+b = 1.5    # float
+c = 'Hello!'    # string
+d = True   # bool
+e = None  # NoneType

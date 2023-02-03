2d2ed67d3d1cc8a8a6c5109cca2c4fbbd341f8b9
Carla Montano <cm.emprendiendo@gmail.com>
2023-02-02 23:03:34 -0600

    sets

---
+++ sets.py
@@ -0,0 +1,20 @@
+# create empty set
+
+newSet = set()
+
+# add elements to the set
+newSet.add(1)
+newSet.add(2)
+newSet.add(3)
+newSet.add(4)
+newSet.add(5)
+newSet.add(5)
+
+print(newSet)
+
+# remove element from a set
+newSet.remove(2)
+print(newSet)
+
+# get the length of a set
+print(f'The set has {len(newSet)} elements.')

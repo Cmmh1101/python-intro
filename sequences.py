1f91407a6a8d96c3e7734d72725b809928482a3f
Carla Montano <cm.emprendiendo@gmail.com>
2023-02-02 22:19:55 -0600

    sequences

---
+++ sequences.py
@@ -0,0 +1,14 @@
+names = ['Harry', 'Ron', 'Hermaiony']
+
+print(names)
+
+# tuple: to group two different values to use them together
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
+# set: unique values
+
+# dict: collection of key value pair (dictionary)
